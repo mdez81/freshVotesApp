d49127225c6a96e03cb0b0b0cb5ed1e41ab84ab3
DESKTOP-H1L6L3U\dezmo <mutz.dezso@yahoo.com>
2023-09-30 13:44:42 +0200

    add missing entities

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/VoteId.java
@@ -0,0 +1,36 @@
+package com.freshvotes.domain;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class VoteId implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6202269445639364170L;
+	
+	private User user;
+	private Feature feature;
+	
+	@ManyToOne
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	@ManyToOne
+	public Feature getFeature() {
+		return feature;
+	}
+	public void setFeature(Feature feature) {
+		this.feature = feature;
+	}
+	
+	
+}
